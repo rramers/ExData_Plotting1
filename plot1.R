c7a0bd191d59a046216f6643d7f096e7323586db
Robert Ramers <ramers@ix.netcom.com>
2016-02-06 09:09:39 -0800

    Plot 1 files

---
+++ plot1.R
@@ -0,0 +1,24 @@
+#Plot 1 code
+#Load in necessary libraries
+library(dplyr)
+#Reads in data from file
+#power <- read.csv("household_power_consumption.txt", header=TRUE,sep =";",na.strings="?",stringsAsFactors = FALSE)
+power <- read.csv("./household_power_consumption.txt", header=TRUE,sep =";",na.strings="?",stringsAsFactors = FALSE)
+#format date fieldlibrary
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+#select dates of interest
+mydf <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+#Convert fields to character and numeric as needed
+mydf$Global_active_power <- as.numeric(as.character(mydf$Global_active_power))
+mydf$Global_reactive_power <- as.numeric(as.character(mydf$Global_reactive_power))
+mydf$Voltage <- as.numeric(as.character(mydf$Voltage))
+#format time information correctly
+mydf <- transform(mydf, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+mydf$Sub_metering_1 <- as.numeric(as.character(mydf$Sub_metering_1))
+mydf$Sub_metering_2 <- as.numeric(as.character(mydf$Sub_metering_2))
+mydf$Sub_metering_3 <- as.numeric(as.character(mydf$Sub_metering_3))
+#plot 1
+hist(mydf$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
+cat("Save Plot1.png to", getwd())
